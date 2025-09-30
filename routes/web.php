5fddae5c5ed1c2e650eba3e2d9d34a45eca6f258
alhafizh24si-make <alhafizh24si@mahasiswa.pcr.ac.id>
2025-09-30 10:16:21 +0700

    Penambahan Form Data Diri

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -41,3 +42,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
 
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
